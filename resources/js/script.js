1db4bd473c4ac545833c807ac4f317cda2dfd2cb
Tiercen <tiercen@gmail.com>
2024-05-08 10:23:59 -0400

    Updated HTML with all sections and content.  Added images. Updated JavaScript to populate the life journey section.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,6 @@
 const workHistorySection = document.getElementById('work-history');
 
-// Fetch JSON data
+// Fetch JSON data work-history
 fetch('./resources/data/work-history.json')
   .then(response => response.json())
   .then(data => {
@@ -51,10 +51,50 @@ fetch('./resources/data/work-history.json')
           toggleLink.textContent = 'Show More';
         }
 
- });
+        });
       
       workHistorySection.appendChild(workCard);
     });
   })
+
+  const lifeEventsSection = document.getElementById('life-events');
+
+  // Fetch JSON data life-events
+  fetch('./resources/data/life-events.json')
+    .then(response => response.json())
+    .then(data => {
+      // Sort data in chronological order
+      data.sort((a, b) => new Date(a.date) - new Date(b.date));
+  
+      // Loop through data and create list items
+      data.forEach(life => {
+          const lifeItem = document.createElement('li');
+          lifeItem.classList.add('life-item');
+
+          let lifeImg = ''; // Initialize empty string for life event image
+
+        // Check if life.logo exists and create image element if it does
+        if (life.img) {
+          // Use backticks for template literals to handle dynamic paths
+          logoHtml = `<img src="${life.img}" alt="${life.event}">`;
+        }
+
+
+        lifeItem.innerHTML = `
+        <div><time>${life.year}</time>
+        <h5 class="life-item-title">${life.event}</h5>
+        <p class="life-item-txt">${life.description}</p>
+        <div>${life.event}</div>
+        </div>
+        
+      `;
+
+      lifeEventsSection.appendChild(lifeItem);
+
+      })
+
+    })
+
+
 
   .catch(error => console.error(error));
